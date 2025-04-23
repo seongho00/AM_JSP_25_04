0f68b5b52a0328fdd263f24366de20a7b7717060
seongho00 <tjdwns000927@gmail.com>
2025-04-23 15:52:05 +0900

    parameter 적용

--- src/main/java/com/KoreaIT/java/AM_jsp/PirntDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PirntDanServlet.java
@@ -15,18 +15,33 @@ public class PirntDanServlet extends HttpServlet {
 
 		response.setContentType("text/html;charset=UTF-8");
 
-		response.getWriter().append("===8단===<br>");
-		response.getWriter().append("8 * 1 = 8<br>");
-		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 1, 8));
-		response.getWriter().append("===8단===");
+//		response.getWriter().append("===8단===<br>");
+//		response.getWriter().append("8 * 1 = 8<br>");
+//		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 1, 8));
+//		response.getWriter().append("===8단===");
 
 //		자바, HTML 을 섞어서 쓸 수 있다.
-		int dan = 8;
-
+//		int dan = 8;
+//
+//		for (int i = 1; i <= 9; i++) {
+//			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+//		}
+		// parameter 보내기 : 주소 뒤에 ?변수이름=값(?dan=8)
+		// 두 개 이상의 parameter 보내기 : 첫번째만 ?, 두번째부터 & (?dan=8&limit=10)
+		//null 값 처리
+		
+		String inputedDan = request.getParameter("dan");
+		
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
+		
+		int dan = Integer.parseInt(inputedDan);
+		
 		for (int i = 1; i <= 9; i++) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
 		}
-
+		
 	}
 
 }
